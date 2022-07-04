ed559221ffa5a460208843066937e1bb417cef0c
srini-45 <107472857+srini-45@users.noreply.github.com>
2022-07-04 12:54:50 +0530

    ooio

---
+++ even odd.py
@@ -0,0 +1,6 @@
+num=int(input("enter a value:"))
+if num%2!=0:
+    print('0dd')
+else:
+    print('even')
+
---
+++ sample.py
@@ -0,0 +1,7 @@
+str="shrinii"
+str1={"1":"blue",
+      "2":"pink",
+      "3":"black"}
+print(type(str1))
+print(len(str))
+print(str1["1"])
